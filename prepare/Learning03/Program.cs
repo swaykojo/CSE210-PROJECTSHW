b57d07b7feedca07cece82323bdc3b0846caccd1
swaykojo <awuahgodsway@gmail.com>
2023-03-08 19:25:13 +0000

    lesson3 updates

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("Hello Learning03 World!"); 
+
+        Fraction fraction = new Fraction(); 
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+
+        Fraction fraction1 = new Fraction(5);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(10, 5); 
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
     }
 }
